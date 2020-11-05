4d85b1c3366db14a522e27412db8510611f0e97d
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-05 18:30:49 -0500

    added cucumber html report plugin to TestRunner

--- src/test/java/com/cybertek/runners/TestRunner.java
+++ src/test/java/com/cybertek/runners/TestRunner.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html: target/cucumber-report.html",
         features = "src/test/resources/features",
-        glue = "com/cybertek/step_definitions",
+        glue = "com/cybertek/step_definitions"
 )
 
 public class TestRunner {
--- src/test/java/com/cybertek/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/LoginStepDefinitions.java
@@ -1,22 +1,28 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefinitions {
 
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
-        System.out.println("I am on the login page");
+        Driver.getDriver().get("https://qa2.vytrack.com");
     }
 
     @When("user logs in as a store manager")
     public void user_logs_in_as_a_store_manager() {
         System.out.println("Trying to log in as a store manager");
     }
+
     @Then("user should be able to see Dashboard page title")
     public void user_should_be_able_to_see_dashboard_page_title() {
-        System.out.println("I see the Dashboard page title");
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Dashboard";
+        Assert.assertEquals("Actual title does not match expected title!", actualTitle, expectedTitle);
     }
 }
