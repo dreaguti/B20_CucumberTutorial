4441f98af438052058858b7abe762a74401a5c07
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-05 18:24:15 -0500

    updated GoogleStepDefinitions

--- src/test/java/com/cybertek/runners/TestRunner.java
+++ src/test/java/com/cybertek/runners/TestRunner.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/cybertek/step_definitions",
-        dryRun = false
 )
 
 public class TestRunner {
--- src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -1,17 +1,25 @@
 package com.cybertek.step_definitions;
 
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
 public class GoogleStepDefinitions {
     @Given("User is on the google search page")
     public void user_is_on_the_google_search_page() {
-        System.out.println("User is on the google search page");
+
+        Driver.getDriver().get("https://www.google.com");
+
     }
 
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
-        System.out.println("User sees the title is Google");
+
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Google";
+
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
 
 }
