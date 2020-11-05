07ab26cabec8143aa23fb90c709f9b6c6e7ac4ca
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-05 18:09:18 -0500

    updated GoogleStepDefinitions

--- src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -6,12 +6,12 @@ import io.cucumber.java.en.Then;
 public class GoogleStepDefinitions {
     @Given("User is on the google search page")
     public void user_is_on_the_google_search_page() {
-        System.out.println("I am on the google search page");
+        System.out.println("User is on the google search page");
     }
 
     @Then("user should see title is Google")
     public void user_should_see_title_is_google() {
-        System.out.println("I see that the title is Google");
+        System.out.println("User sees the title is Google");
     }
 
 }
