eb27c29b02acbe95b4e425eecdd82c1e2587d24d
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-05 18:54:54 -0500

    updated GoogleStepDefinitions

--- src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/GoogleStepDefinitions.java
@@ -8,7 +8,7 @@ import org.junit.Assert;
 public class GoogleStepDefinitions {
     @Given("User is on the google search page")
     public void user_is_on_the_google_search_page() {
-
+        //Assert.fail("failing test on purpose"); -- just to see it on report
         Driver.getDriver().get("https://www.google.com");
 
     }
@@ -18,7 +18,6 @@ public class GoogleStepDefinitions {
 
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "Google";
-
         Assert.assertEquals("Actual title doesn't match expected title!", actualTitle, expectedTitle);
     }
 
