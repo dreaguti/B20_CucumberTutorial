6ce5146cefcdfd7e3c32338c2e12c3ff0a03c4ff
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-05 18:56:27 -0500

    updated LoginStepDefinitions

--- src/test/java/com/cybertek/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/LoginStepDefinitions.java
@@ -9,19 +9,19 @@ import org.junit.Ignore;
 
 @Ignore
 public class LoginStepDefinitions {
-    @Ignore
+
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
         Driver.getDriver().get("https://qa2.vytrack.com");
     }
-    @Ignore
+
     @When("user logs in as a store manager")
     public void user_logs_in_as_a_store_manager() {
         String actualTitle = Driver.getDriver().getTitle();
         String expectedTitle = "Login";
         Assert.assertEquals("Actual title does not match expected title!", actualTitle, expectedTitle);
     }
-    @Ignore
+
     @Then("user should be able to see Dashboard page title")
     public void user_should_be_able_to_see_dashboard_page_title() {
         //String actualTitle = Driver.getDriver().getTitle();
