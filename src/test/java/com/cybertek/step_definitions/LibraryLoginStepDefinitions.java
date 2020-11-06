fdd037949881b3b83a219bdb032ebaf58a10aaec
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-05 21:31:53 -0500

    added LibraryLoginStepDefinitions

--- src/test/java/com/cybertek/step_definitions/LibraryLoginStepDefinitions.java
+++ src/test/java/com/cybertek/step_definitions/LibraryLoginStepDefinitions.java
@@ -1,4 +1,22 @@
 package com.cybertek.step_definitions;
 
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class LibraryLoginStepDefinitions {
+    @Given("user is on the login page")
+    public void user_is_on_the_login_page() {
+
+    }
+
+    @When("user logs in as a librarian")
+    public void user_logs_in_as_a_librarian() {
+
+    }
+
+    @Then("user should be able to see dashboard")
+    public void user_should_be_able_to_see_dashboard() {
+
+    }
 }
