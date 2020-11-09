01a79016c2c99d382c8193c1d765e33fac44f258
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-09 16:08:08 -0500

    added EtsySearch feature file and step definitions

---
+++ src/test/java/EtsyStepDefinitions.java
@@ -0,0 +1,20 @@
+import com.cybertek.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class EtsyStepDefinitions {
+    //Windows shortcut - Alt+Enter
+
+    @Given("user is on the etsy home page")
+    public void userIsOnTheEtsyHomePage() {
+        Driver.getDriver().get("https://www.etsy.com");
+    }
+
+    @Then("user should see title is as expected")
+    public void userShouldSeeTitleIsAsExpected() {
+        String actualTitle=Driver.getDriver().getTitle();
+        String expectedTitle="Etsy - Shop for handmade, vintage, custome, and unique gifts for everyone";
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+}
---
+++ src/test/resources/features/EtsySearch.feature
@@ -0,0 +1,10 @@
+Feature: Etsy search feature
+  Agile story: User should be able to enter search terms
+  and see relevant results in the page and in the title.
+
+  @etsyWip
+  Scenario: Title verification
+    Given user is on the etsy home page
+    Then user should see title is as expected
+
+    #expected: Etsy - Shop for handmade, vintage, custome, and unique gifts for everyone
