9dc1916b8755fb2a3f79ad1682e58e0e12d51b56
Andrea Gutierrez <drea.guti@gmail.com>
2020-11-09 18:47:57 -0500

    added EtsySearch feature file and step definitions

--- src/test/java/EtsyStepDefinitions.java
+++ src/test/java/EtsyStepDefinitions.java
@@ -7,14 +7,14 @@ public class EtsyStepDefinitions {
     //Windows shortcut - Alt+Enter
 
     @Given("user is on the etsy home page")
-    public void userIsOnTheEtsyHomePage() {
+    public void user_is_on_the_etsy_home_page() {
         Driver.getDriver().get("https://www.etsy.com");
     }
 
     @Then("user should see title is as expected")
-    public void userShouldSeeTitleIsAsExpected() {
-        String actualTitle=Driver.getDriver().getTitle();
-        String expectedTitle="Etsy - Shop for handmade, vintage, custom, and unique gifts for everyone";
+    public void user_should_see_title_is_as_expected() {
+        String actualTitle = Driver.getDriver().getTitle();
+        String expectedTitle = "Etsy - Shop for handmade, vintage, custom, and unique gifts for everyone";
         Assert.assertEquals(actualTitle, expectedTitle);
     }
 }
